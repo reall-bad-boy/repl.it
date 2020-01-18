4312dd15c479e6503dfaaaed94f9904b61862d9e
Glitch (hello-express) <none>
2020-01-18 16:35:15 +0000

    📷🕋 Checkpoint ./server.js:4981465/21822 ./package.json:4981465/9695 ./config.js:4981465/298

--- config.js
+++ config.js
@@ -1,11 +1,6 @@
 module.exports = {
-
-    TOKEN: '',
-
-    YT_API_KEY: 'AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4',
-
-    prefix: '',
-
-    devs: ['593585813648965633']
-
+    TOKEN: 'Token',
+    YT_API_KEY: 'AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4',
+    prefix: '',
+    devs: ['593585813648965633']
 }
