72badfc30f1af78d4602bb789aee9eb13e015aab
Glitch (hello-express) <none>
2020-08-27 21:04:43 +0000

    🕍🛳 Checkpoint ./server.js:7772686/30

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ client.on("ready", () => {
   let statuses = [
     `Servers: ${client.guilds.size} | Users: ${client.users.size}`,
     `MADE BY PABLO`,
-     `BRAM TO MAZAY `
+     `||PABLO|| `
   ];
   setInterval(function() {
     let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
