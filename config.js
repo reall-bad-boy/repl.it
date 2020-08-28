1be922924e57b10aaae58cec88a8470c55572cf9
Glitch (hello-express) <none>
2020-08-28 07:16:38 +0000

    🎀📺 Checkpoint ./server.js:7772686/1 ./config.js:7772686/36

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
     TOKEN: 'NzQ4NjM3NDE1MTA2OTM2OTI3.X0gU7A.OeFDu8nLRL3dvcZvbswAvTtgmJU',
     YT_API_KEY: 'AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4',
     prefix: '!',
-    devs: ['660527960922521632']
+    devs: ['619039917615480832']
 }
--- server.js
+++ server.js
@@ -53,7 +53,7 @@ client.on("ready", () => {
   let statuses = [
     `Servers: ${client.guilds.size} | Users: ${client.users.size}`,
     `MADE BY PABLO`,
-     `!help`
+     `!1help`
   ];
   setInterval(function() {
     let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
