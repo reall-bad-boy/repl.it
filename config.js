0166bb2a236a58beab5a48d5be593e2ba7b3519f
Glitch (splashy-charmed-calf) <none>
2020-09-22 08:34:18 +0000

    🐪🏠 Checkpoint ./config.js:17969920/59

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TOKEN: '',
+    TOKEN: 'NzQ4NjM3NDE1MTA2OTM2OTI3.X0gU7A.OeFDu8nLRL3dvcZvbswAvTtgmJU',
     YT_API_KEY: 'AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4',
     prefix: '=',
     devs: ['619039917615480832']
