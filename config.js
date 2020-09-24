fb3fe503c3b5834b17fb7baa9bc50e26a3c02e62
Glitch (splashy-charmed-calf) <none>
2020-09-24 20:35:34 +0000

    👕🐊 Checkpoint ./config.js:16885247/120

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TOKEN: 'NzU3ODk1MzA0NTE0MTA5NTAw.X2nDAQ.pO8GIdRpQoHehK-RwPJtAlbo2S8',///تۆکین دانی
+    TOKEN: 'NzU4Nzg2MTYzNTIzMTkwNzk0.X20Arg.WOo9uD8YK-wJV_6oRwh5TdiZLxI',///تۆکین دانی
     YT_API_KEY: 'AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4',
     prefix: 'c!',///کۆماند دانێ
     devs: ['694223945049702440']
