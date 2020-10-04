429d47d9a67779f355645ba31efd250df9e16426
Glitch (splashy-charmed-calf) <none>
2020-10-04 03:03:30 +0000

    🎓🌞 Checkpoint ./config.js:23335165/59 ./server.js:23335165/1432

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TOKEN: '',///تۆکین دانی
+    TOKEN: 'NzYxNDg2MTMxNzE1NTA2MTg3.X3bTOQ.KDBvJ_hWhP8vc8r6SyjYt9-Nuv8',///تۆکین دانی
     YT_API_KEY: 'AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4',
     prefix: '=',///کۆماند دانێ
     devs: ['670647563627659306']
