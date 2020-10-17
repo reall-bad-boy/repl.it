0b9dabfa26315228a025b25ab79d2f3e6adfe2cb
Glitch (splashy-charmed-calf) <none>
2020-10-17 07:44:46 +0000

    👜✌️ Checkpoint ./server.js:23877998/1278

--- server.js
+++ server.js
@@ -1088,7 +1088,24 @@ message.channel.send(`**✅ The AntiJoin Is __𝐎𝐍__ !**`)
  
         })
  
- 
+ const AntiSpam = require("discord-anti-spam");
+const antiSpam = new AntiSpam({
+  warnThreshold: 3, // Amount of messages sent in a row that will cause a warning.
+  kickThreshold: 7, // Amount of messages sent in a row that will cause a ban.
+  banThreshold: 7, // Amount of messages sent in a row that will cause a ban.
+  maxInterval: 2000, // Amount of time (in milliseconds) in which messages are considered spam.
+  warnMessage: "{@user}, please stop spamming .", // Message that will be sent in chat upon warning a user.
+  kickMessage: "**{user_tag}**, kicked for spam .", // Message that will be sent in chat upon kicking a user.
+  banMessage: "**{user_tag}**, banned for spam .", // Message that will be sent in chat upon banning a user.
+  maxDuplicatesWarning: 7, // Amount of duplicate messages that trigger a warning.
+  maxDuplicatesKick: 10, // Amount of duplicate messages that trigger a warning.
+  maxDuplicatesBan: 12, // Amount of duplicate messages that trigger a warning.
+  exemptPermissions: ["ADMINISTRATOR"], // Bypass users with any of these permissions.
+  ignoreBots: true, // Ignore bot messages.
+  verbose: true, // Extended Logs from module.
+  ignoredUsers: [] // Array of User IDs that get ignored.
+  // And many more options... See the documentation.
+});
 
  
 client.on('message', message => {
