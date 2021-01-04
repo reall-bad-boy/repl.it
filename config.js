631a8367899f645b1842439ac9093baa9c4aa3e1
Glitch (splashy-charmed-calf) <none>
2021-01-04 20:26:09 +0000

    🏆🏮 Checkpoint ./config.js:25383252/23

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     TOKEN: 'Nzk1NjQxNTMyMTUyNjc2Mzcy.X_MU6w.938bHM58vLtJISh2YF8mOKi8VuE',///تۆکین دانی
     YT_API_KEY: 'AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4',
-    prefix: 'b/',///کۆماند دانێ
+    prefix: '$',///کۆماند دانێ
     devs: ['670647563627659306']
 }
