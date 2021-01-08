55fcb09400b5aff2354cce76c617b23ade9d81bc
Glitch (splashy-charmed-calf) <none>
2021-01-08 13:07:21 +0000

    ⏰🎥 Checkpoint ./config.js:25383252/118

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TOKEN: 'NzczNjY0NTU1MTI0Nzg1MTgy.X6MhQw.mduEZG1iuyq4s_aLwBSDwOQBAPQ',///تۆکین دانی
+    TOKEN: 'Nzk3MDg4NTkwOTk1NjUyNjM4.X_hYmQ.y_in1RgKRP2EbzwkzjJbGV2FVjw',///تۆکین دانی
     YT_API_KEY: 'AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4',
     prefix: '$',///کۆماند دانێ
     devs: ['793499843762454529']
