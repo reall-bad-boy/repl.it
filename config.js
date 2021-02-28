b1adbf119399c6903ff2d39d0cf34cc096d3faca
Glitch (splashy-charmed-calf) <none>
2021-02-28 13:48:34 +0000

    🏣🏤 Checkpoint ./config.js:28927045/136

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    TOKEN: 'Nzk3MDg4NTkwOTk1NjUyNjM4.X_hYmQ.y_in1RgKRP2EbzwkzjJbGV2FVjw',///تۆکین دانی
+    TOKEN: 'Nzc2MDQzNjg5MzM2MTExMTA0.X6vJAQ.l2bPgUIkoRXu-9k-RyxD-Ok5jlE',///تۆکین دانی
     YT_API_KEY: 'AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4',
     prefix: '$',///کۆماند دانێ
-    devs: ['793499843762454529']
+    devs: ['']
 }
